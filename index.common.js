ec5dee602dd006b3b98a5b8658cd047eb556fe83
sheldon <sheldon@textemma.com>
2017-03-22 15:45:59 -0400

    use smaller phone

--- index.common.js
+++ index.common.js
@@ -1,20 +1,20 @@
 'use strict'
 
 var React = require('react')
-var metadata = require('libphonenumber-js/metadata.min.json')
+var metadata = require('meta.min.json')
 
 var Input = require('./custom').default
 
 var Phone = function Phone(props)
 {
-   var properties = Object.keys(props).reduce(function(reduced, property)
-   {
-   	reduced[property] = props[property]
-   	return reduced
-   },
-   { metadata: metadata })
-
-	return React.createElement(Input, properties)
+  var properties = Object.keys(props).reduce(function(reduced, property)
+  {
+    reduced[property] = props[property]
+    return reduced
+  },
+  { metadata: metadata })
+  
+  return React.createElement(Input, properties)
 }
 
 exports = module.exports = Phone
@@ -23,23 +23,23 @@ var custom = require('libphonenumber-js/custom')
 
 exports.parse_phone_number = function parse()
 {
-	var parameters = Array.prototype.slice.call(arguments)
-	parameters.push(metadata)
-	return custom.parse.apply(this, parameters)
+  var parameters = Array.prototype.slice.call(arguments)
+  parameters.push(metadata)
+  return custom.parse.apply(this, parameters)
 }
 
 exports.format_phone_number = function format()
 {
-	var parameters = Array.prototype.slice.call(arguments)
-	parameters.push(metadata)
-	return custom.format.apply(this, parameters)
+  var parameters = Array.prototype.slice.call(arguments)
+  parameters.push(metadata)
+  return custom.format.apply(this, parameters)
 }
 
 exports.is_valid_phone_number = function is_valid_number()
 {
-	var parameters = Array.prototype.slice.call(arguments)
-	parameters.push(metadata)
-	return custom.isValidNumber.apply(this, parameters)
+  var parameters = Array.prototype.slice.call(arguments)
+  parameters.push(metadata)
+  return custom.isValidNumber.apply(this, parameters)
 }
 
 // camelCase aliases
